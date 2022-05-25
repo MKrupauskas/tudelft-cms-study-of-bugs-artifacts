6f27b4e7815ea282a17582a844bb871190ecb0a7
Mykolas Krupauskas <mkrupauskas@gmail.com>
2022-05-25 14:52:07 +0300

    change puppet commit collection

--- puppet/commits.sh
+++ puppet/commits.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-cd ..
+# clone the puppet repo if that's not already done
 # git clone git@github.com:puppetlabs/puppet.git
 cd puppet
 
-../tudelft-research/git-json.sh > commits.json
+../git-json.sh > commits.json
 
-mv commits.json ../tudelft-research/commits.json
-cd ../tudelft-research
+mv commits.json ../commits.json
+cd ..
