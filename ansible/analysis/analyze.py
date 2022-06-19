9681632d8ad649f2c96e60bae661d590f0a6898f
Matas Rastenis <matas2k@gmail.com>
2022-06-19 13:42:16 +0300

    Categorization results

--- ansible/analysis/analyze.py
+++ ansible/analysis/analyze.py
@@ -3,6 +3,7 @@ import os
 import sys
 import csv
 
+totalCount=0
 
 with open("results/categorized.tsv") as categorizedFile:
     categorizedBugs = csv.reader(categorizedFile, delimiter="\t", quotechar='"')
@@ -26,11 +27,13 @@ with open("results/categorized.tsv") as categorizedFile:
             print("Skipping iteration boundary")
             continue
 
+        totalCount=totalCount+1
+
         (
             bugUrl,
             fixUrl,
-            symptomCode,
-            rootCauseCode,
+            symptom,
+            rootCause,
             impactSeverity,
             impactConsequence,
             codeFix,
@@ -41,24 +44,23 @@ with open("results/categorized.tsv") as categorizedFile:
             *rest,
         ) = categorizedBug
 
-        print(
-            bugUrl,
-            fixUrl,
-            symptomCode,
-            rootCauseCode,
-            impactSeverity,
-            impactConsequence,
-            codeFix,
-            conceptualFix,
-            systemDependency,
-            triggerCause,
-            triggerReproduction,
-            rest,
-        )
+        # Aggregate counts
+        results["symptoms"][symptom] = results["symptoms"].get(symptom, 0) + 1
+        results["root_causes"][rootCause] = results["root_causes"].get(rootCause, 0) + 1
+        results["impact_severities"][impactSeverity] = results["impact_severities"].get(impactSeverity, 0) + 1
+        results["impact_consequences"][impactConsequence] = results["impact_consequences"].get(impactConsequence, 0) + 1
+        results["code_fixes"][codeFix] = results["code_fixes"].get(codeFix, 0) + 1
+        results["conceptual_fixes"][conceptualFix] = results["conceptual_fixes"].get(conceptualFix, 0) + 1
+        results["system_dependencies"][systemDependency] = results["system_dependencies"].get(systemDependency, 0) + 1
+        results["trigger_causes"][triggerCause] = results["trigger_causes"].get(triggerCause, 0) + 1
+        results["trigger_reproductions"][triggerReproduction] = results["trigger_reproductions"].get(triggerReproduction, 0) + 1
 
-        results = results.get(key, default=None)
+        #results["system_dependencies_systems"][symptom] = results["system_dependencies_systems"].get(symptom, 0) + 1
+
 
     # Write aggregation
     with open(f"results/categorization_results.json", "w", encoding="utf-8") as f:
         json.dump(results, f, indent=4)
         print(json.dumps(results, indent=4))
+
+    print("Wrote aggregation for {} bugs.".format(totalCount))
