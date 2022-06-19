a8b4b87968d8e0cdba17e8c1cd0aceaa89c51703
Matas Rastenis <matas2k@gmail.com>
2022-06-19 13:34:04 +0300

    Formatting

--- ansible/analysis/analyze.py
+++ ansible/analysis/analyze.py
@@ -4,31 +4,61 @@ import sys
 import csv
 
 
-
-
 with open("results/categorized.tsv") as categorizedFile:
     categorizedBugs = csv.reader(categorizedFile, delimiter="\t", quotechar='"')
 
     # For aggregating amounts
-    results = dict()
-
+    results = {
+        "symptoms": {},
+        "root_causes": {},
+        "impact_severities": {},
+        "impact_consequences": {},
+        "code_fixes": {},
+        "conceptual_fixes": {},
+        "system_dependencies": {},
+        "system_dependencies_systems": {},
+        "trigger_causes": {},
+        "trigger_reproductions": {},
+    }
 
     for categorizedBug in categorizedBugs:
-        if len(row) == 0:
+        if len(categorizedBug) == 0:
             print("Skipping iteration boundary")
             continue
-        print(row)
 
-        for bugUrl, fixUrl, symptomCode, rootCauseCode, impactSeverity, impactConsequence, codeFix, conceptualFix, systemDependency, triggerCause, triggerReproduction, operatingSystem in row:
-            print(bugUrl, fixUrl, symptomCode, rootCauseCode, impactSeverity, impactConsequence, codeFix, conceptualFix, systemDependency, triggerCause, triggerReproduction, operatingSystem)
-        
+        (
+            bugUrl,
+            fixUrl,
+            symptomCode,
+            rootCauseCode,
+            impactSeverity,
+            impactConsequence,
+            codeFix,
+            conceptualFix,
+            systemDependency,
+            triggerCause,
+            triggerReproduction,
+            *rest,
+        ) = categorizedBug
 
+        print(
+            bugUrl,
+            fixUrl,
+            symptomCode,
+            rootCauseCode,
+            impactSeverity,
+            impactConsequence,
+            codeFix,
+            conceptualFix,
+            systemDependency,
+            triggerCause,
+            triggerReproduction,
+            rest,
+        )
+
+        results = results.get(key, default=None)
 
     # Write aggregation
     with open(f"results/categorization_results.json", "w", encoding="utf-8") as f:
         json.dump(results, f, indent=4)
         print(json.dumps(results, indent=4))
-
-
-
-
